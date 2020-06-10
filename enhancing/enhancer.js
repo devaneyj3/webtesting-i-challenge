2adffec28b661c4614c0f3d48febd1e727380d0d
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-06-09 21:09:26 -0400

    first test passes'

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -14,7 +14,7 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  return item.durabillity = 100;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,11 @@
 const enhancer = require('./enhancer.js');
+
+test('Durabillity resets to 100', () => { 
+    const item = {
+        durabillity: 59
+    }
+    const durabillity = enhancer.repair(item)
+    expect(durabillity).toBe(100)
+
+}) 
 // test away!
